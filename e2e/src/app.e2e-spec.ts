8479d338fe870294e790dc8715596f1ee34c3765
Elmer Urrea Zuluaga - Ceiba Software <elmer.urrea@ceiba.com.co>
2019-03-05 09:15:01 -0500

    Implementar pruebas end_to_end (e2e)

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, logging, element, by } from 'protractor';
 
 describe('workspace-project App', () => {
   let page: AppPage;
@@ -8,9 +8,53 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('should display TRM value', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to estacionamiento!');
+    expect(element(by.id('trmHoy')).getText()).not.toBe('');
+  });
+
+  it('should insert motocycle and list on the table', () => {
+    page.navigateTo();
+    element(by.id('btnRegistrarMoto')).click();
+    element(by.id('placa')).sendKeys('RFG534');
+    element(by.id('cilindraje')).sendKeys(125);
+    element(by.id('btnRegistrar')).click();
+    //TODO validar que se muestre la info en la tabla
+    //TODO ajustar refresco
+    page.navigateTo();
+    element(by.id('inputBuscar')).sendKeys('RFG534');
+    element(by.className('btnSalida')).click();
+  });
+
+  it('should insert car and list on the table', () => {
+    page.navigateTo();
+    element(by.id('btnRegistrarCarro')).click();
+    element(by.id('placa')).sendKeys('MNJ721');
+    expect(element(by.id('cilindraje')).isPresent()).toBe(false);
+    element(by.id('btnRegistrar')).click();
+    //TODO validar que se muestre la info en la tabla
+    //TODO ajustar refresco
+    page.navigateTo();
+    element(by.id('inputBuscar')).sendKeys('MNJ721');
+    element(by.className('btnSalida')).click();
+  });
+
+  it('should mark exit of vehicule and pay correct value', () => {
+    page.navigateTo();
+    //ingresar vehiculo
+    element(by.id('btnRegistrarCarro')).click();
+    element(by.id('placa')).sendKeys('HPY721');
+    element(by.id('btnRegistrar')).click();
+    //salida vehiculo
+    element(by.id('inputBuscar')).sendKeys('HPY721');
+    //TODO validar solo encontrar uno
+    //TODO ajustar refresco
+    page.navigateTo();
+    element(by.className('btnSalida')).click();
+    expect(element(by.id('lblPlacaPago')).getText()).toEqual('HPY721');
+    expect(element(by.id('lblFechaInicioPago')).getText()).not.toBe('');
+    expect(element(by.id('lblFechaFinPago')).getText()).not.toBe('');
+    expect(element(by.id('lblValorPagar')).getText()).toEqual('$1,000.00');
   });
 
   afterEach(async () => {
